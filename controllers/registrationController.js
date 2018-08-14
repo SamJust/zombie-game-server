27c8c87375b592875bf605b91081d3c853049542
SamJust <samjustanimstudio@gmail.com>
2018-08-14 17:20:26 +0300

    [36] moar registration validation

--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -8,16 +8,27 @@ let Formula = mongoose.model('formulas');
 module.exports = (app)=>{
 
   app.post('/registration', (req, res)=>{
-    
     const emailRegExp = new RegExp(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/, 'i');
     const usernameRegExp = new RegExp(/^[a-zA-Z0-9]+$/);
     const passwordRegExp = new RegExp(/^[a-zA-Z0-9]+$/);
     const emptySpacesRegExp = new RegExp(/[\s]{2,}/);
+    const passwordEmptySpacesRegExp = new RegExp(/[\s]{2,}/);
 
-    if(!emailRegExp.test(req.body.email) ||
-       !usernameRegExp.test(req.body.nickname) ||
-        emptySpacesRegExp.test(req.body.nickname) ||
-       !passwordRegExp.test(req.body.password)) return res.sendStatus(400);
+    if (!emailRegExp.test(req.body.email)) {
+      return res.status(400).send('emailNoMatchCriteria');
+    }
+
+    if (!usernameRegExp.test(req.body.nickname) || emptySpacesRegExp.test(req.body.nickname)) {
+      return res.status(400).send('usernameNoMatchCriteria1');
+    } else if (req.body.nickname.length < 3 || req.body.nickname.length > 36) {
+      return res.status(400).send('usernameNoMatchCriteria2');
+    }
+
+    if (!passwordRegExp.test(req.body.password)) {
+      return res.status(400).send('passwordNoMatchCriteria');
+    } else if (req.body.password.length < 6 || req.body.password.length > 32) {
+      return res.status(400).send('passwordNoMatchCriteria');
+    }
 
     User.findOne({email:req.body.email}).then((data)=>{
       if(!data){
