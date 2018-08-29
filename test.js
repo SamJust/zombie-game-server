638927bd9f7f88e2a5e03f0ea928cecc9a8342ba
SamJust <samjustanimstudio@gmail.com>
2018-08-29 15:07:33 +0300

    login registration tests

--- test.js
+++ test.js
@@ -3,6 +3,14 @@ const request = require('./utils/testRequest')
 
 const app = require('./config/app.js');
 
+const localhost = 'http://localhost:3000';
+
+const testUser = {
+    email: 'testuser@samjust.com',
+    nickname: 'testUser',
+    password: 'samjust'
+};
+
 
 mongoose.connect("mongodb://admin:admin@ds231559.mlab.com:31559/zombie-game-app").then(
     ()=>{
@@ -10,19 +18,121 @@ mongoose.connect("mongodb://admin:admin@ds231559.mlab.com:31559/zombie-game-app"
         const server = app.listen(3000, async () => {
             console.log(`Server for test's on port ${3000}`);
             
+            // An invalid login/pass test
             await request({
-                url: 'http://localhost:3000/api/login',
+                url: `${localhost}/api/login`,
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body:JSON.stringify({
-                    email: 'testuser@test.test',
-                    password: 'samjust'
+                    email: testUser.email,
+                    password: testUser.password
                 })
             })
-                .statusCode(404)
+                .setDescription('Check what if user is not registrated')
+                .expectedStatusCode(404)
                 .exec();
+
+            // Email doesn't pass validation        
+            await request({
+                url:`${localhost}/api/registration`,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body:JSON.stringify({
+                    email:'wrongEmail.com',
+                    nickname: testUser.nickname,
+                    password: testUser.password
+                })
+            })
+                .setDescription('Check what if email is invalid')
+                .expectedResponseBody('emailNoMatchCriteria')
+                .exec();
+
+            // Nickname is too short        
+            await request({
+                url:`${localhost}/api/registration`,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body:JSON.stringify({
+                    email:testUser.email,
+                    nickname: '2s',
+                    password: testUser.password
+                })
+            })
+                .setDescription('Check what if nickname is too short')
+                .expectedResponseBody('usernameNoMatchCriteria')
+                .exec();
+
+            // Nickname is too long        
+            await request({
+                url:`${localhost}/api/registration`,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body:JSON.stringify({
+                    email:testUser.email,
+                    nickname: 'itsJustAVeryVeryLongCameCasedStringIMPuttingHereCauseWellINeedOne',
+                    password: testUser.password
+                })
+            })
+                .setDescription('Check if nickname is too long')
+                .setDescription('Check what if nickname is too long')
+                .expectedResponseBody('usernameNoMatchCriteria')
+                .exec();    
+
+            // Password is too short        
+            await request({
+                url:`${localhost}/api/registration`,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body:JSON.stringify({
+                    email:testUser.email,
+                    nickname: testUser.nickname,
+                    password: '12345'
+                })
+            })
+                .setDescription('Check what if password is too short')
+                .expectedResponseBody('passwordNoMatchCriteria')
+                .exec();
+                
+            // Password is too long        
+            await request({
+                url:`${localhost}/api/registration`,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body:JSON.stringify({
+                    email:testUser.email,
+                    nickname: testUser.nickname,
+                    password: 'aVeryVeryVeryVeryVeryVeryLongLongLongLongLongPassword'
+                })
+            })
+                .setDescription('Check what if password is too long')
+                .expectedResponseBody('passwordNoMatchCriteria')
+                .exec();    
+
+            // Successful registration            
+            // await request({
+            //     url:`${localhost}/api/registration`,
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json'
+            //     },
+            //     body:JSON.stringify({
+            //         email:testUser.email,
+            //         nickname: testUser.nickname,
+            //         password: testUser.password
+            //     })
+            // }).exec();
         
             server.close(() => console.log('close'));
         });
--- utils/testRequest.js
+++ utils/testRequest.js
@@ -1,52 +1,70 @@
 const request = require('request');
 
 module.exports = (url) => {
-    let responseType, statusCode,
-        responseObject;
-    const testRequest = {
+    return {
 
-        statusCode: (code) => {
-            statusCode = code;
-            return testRequest;
+        expectedStatusCode (code){
+            this.statusCode = code;
+            return this;
         },
 
-        responseType: (type)=>{
-            responseType = type;
-            return testRequest;
+        expectedResponseType(type){
+            this.responseType = type;
+            return this;
         },
 
-        responseObject: (object) => {
-            responseObject = object;
-            return testRequest;
+        expectedResponseObject(object){
+            this.responseObject = object;
+            return this;
         },
 
-        exec: () => new Promise((resolve, reject)=>{
-            request(url, (err, headers, data)=>{
-                if(err) reject(err);                
+        expectedResponseBody(body){
+            this.responseBody = body;
+            return this;
+        },
 
-                if(headers.headers["content-type"].includes('application/json')){
-                    data = JSON.parse(data);
-                }
+        setDescription(description){
+            this.description = description;
+            return this;
+        },
 
-                if(statusCode){
-                    if(headers.statusCode !== statusCode) reject(`Status codes didn't match!`)
-                }
+        exec () {
+            return new Promise((resolve, reject)=>{
+                request(url, (err, httpResponse, data)=>{
+                    if(err) reject(err);
+                    
+                    const { responseObject, responseType, statusCode,
+                            responseBody, description } = this;
+                    
 
-                if(responseType){
-                    if(typeof data !== responseType) reject(`Wrong type of data returned`);
-                }
+                    if(httpResponse.headers["content-type"].includes('application/json')){
+                        data = JSON.parse(data);
+                    }
 
-                if(responseObject){
-                    for (const key in responseObject) {
-                        if (responseObject.hasOwnProperty(key)) {
-                            if(typeof data[key] !== responseObject[key]) reject(`Type of ${key} didn't match`)
+                    if(statusCode){
+                        if(httpResponse.statusCode !== this.statusCode) reject(`Status codes didn't match! ${description}`)
+                    }
+
+                    if(responseType){
+                        console.log(responseType)
+                        if(typeof data !== responseType) reject(`Wrong type of data returned. ${description}`);
+                    }
+
+                    if(responseObject){
+                        for (const key in responseObject) {
+                            if (responseObject.hasOwnProperty(key)) {
+                                if(typeof data[key] !== responseObject[key]) reject(`Type of ${key} didn't match. ${description}`)
+                            }
                         }
                     }
-                }
 
-                resolve(headers, data);
-            });
-        })
+                    if(responseBody){
+                        if(data !== responseBody) reject(`The response body was unexpected. ${description}`);
+                    }
+
+                    resolve(httpResponse, data);
+                });
+            })
+        }
     }
-    return testRequest;
 };
